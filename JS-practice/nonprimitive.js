0e2d13b0a19d94325fa1642543e7fea68e78146c
FahadShaik <shaikfahad904@gmail.com>
2024-01-25 12:25:24 +0530

    created an non primitive data type in JS practice

---
+++ JS-practice/nonprimitive.js
@@ -0,0 +1,19 @@
+// crud style
+// Non primitive type of data (array is also a type of object), (Object is also a type of object)
+
+//create (array)
+var duffleBag = ["Tommy hilfiger", true, 2300];
+// console.log(duffleBag);
+// console.log(typeof duffleBag);
+
+//create Object
+var overNighter = {
+  brandName: "Tommy Hilfiger",
+  rating: 4.4,
+  bestOffers: undefined,
+  cod: true,
+};
+
+// read object
+console.log(overNighter);
+console.log(typeof overNighter);
