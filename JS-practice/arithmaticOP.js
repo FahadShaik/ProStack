e2562afef08fb3b47912e701c48939f264a660d7
FahadShaik <shaikfahad904@gmail.com>
2024-01-27 19:09:08 +0530

    Operators and output behaviour

---
+++ JS-practice/arithmaticOP.js
@@ -0,0 +1,22 @@
+var a = 10;
+var b = "20";
+var c = "rahul";
+var d = true;
+var e = 40.5;
+var f;
+
+console.log(a, b, c, d, e);
+console.log(typeof a, typeof b, typeof c, typeof d, typeof e, typeof f);
+// Add
+console.log(a + b);
+console.log(a + c);
+console.log(a + d);
+console.log(a + e);
+console.log(a + f);
+
+// multiply
+console.log(a * b);
+console.log(a * c);
+console.log(a * d);
+console.log(a * e);
+console.log(a * f);
