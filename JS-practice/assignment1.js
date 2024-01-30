185fa06eb3f42445387d5423985c834475305b42
FahadShaik <shaikfahad904@gmail.com>
2024-01-30 16:00:48 +0530

    Some more assignment problems

--- JS-practice/assignment1.js
+++ JS-practice/assignment1.js
@@ -22,3 +22,46 @@ if (multipleThree % 3 === 0) {
   console.log("Tata bye bye , Khatam gaya.");
 }
  */
+
+//Write a program to check if a number is positive or not?
+/* const positiveNum = parseInt(prompt("Enter a digit: "));
+
+if (positiveNum > 0) {
+  console.log("thank god!num is a positive not corona's checkup");
+} else {
+  console.log("number negative");
+} */
+
+//Write a program to check if a number is 3-digit numbers or not?
+
+// const threeDigitNum = parseInt(prompt("Enter a digit: "));
+
+// if (threeDigitNum >= 100) {
+//   console.log("this is three digit number");
+// } else {
+//   console.log("no this is not 3 digit number");
+// }
+
+//  Write a program to print even or odd for a given number?
+
+/* const evenOrOdd = parseInt(prompt("Enter a digit: "));
+if (evenOrOdd % 2 === 0) {
+  console.log("The number is even");
+} else {
+  console.log("the number is odd");
+} */
+
+//26. Write a program to print the greatest number in given two numbers?
+
+// const num1 = parseInt(prompt("Enter a digit 1: "));
+// const num2 = parseInt(prompt("Enter a digit 2: "));
+
+// if (num2 > num1) {
+//   console.log(`${num2} is greater`);
+// }
+// if (num1 > num2) {
+//   console.log(`${num1} is greater`);
+// }
+// if (num1 === num2) {
+//   console.log(`${num1} and ${num2} are equal`);
+// }
