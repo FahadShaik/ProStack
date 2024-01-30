c1ddeb1b263826ca94c2ee87caa0cecd09622f2e
FahadShaik <shaikfahad904@gmail.com>
2024-01-30 15:25:14 +0530

    new 2 assignment

---
+++ JS-practice/assignment1.js
@@ -0,0 +1,24 @@
+var prompt = require("prompt-sync")();
+
+/* Write a program to check if a number is divisible by 7 or not? */
+
+/* const num = parseInt(prompt("Enter a number"));
+
+if (num % 7 === 0) {
+  console.log("num divisible by 7");
+} else {
+  console.log("num not divisible");
+}
+ */
+
+//Write a program to check if a number is multiple of 3 or not?
+
+/* 
+const multipleThree = parseInt(prompt("enter a number: "));
+
+if (multipleThree % 3 === 0) {
+  console.log("the number is multiple of 3");
+} else {
+  console.log("Tata bye bye , Khatam gaya.");
+}
+ */
