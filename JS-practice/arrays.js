06380b128ddb64a4f044bc1bbc7963404075cae2
FahadShaik <shaikfahad904@gmail.com>
2024-01-31 14:23:47 +0530

    The looping of array and writing of array

---
+++ JS-practice/arrays.js
@@ -0,0 +1,19 @@
+// Performing crud style learning on JS Arrays
+
+//create
+const arrayOne = [];
+const arrayTwo = [1, "anny", {}, [], null, undefined, true];
+
+// console.log(arrayTwo.length);
+
+//looping over an array using for loop
+for (let i = 0; i < arrayTwo.length; i++) {
+  console.log("ArrayValue: ", arrayTwo[i]);
+  console.log(i);
+}
+
+// looping an array with es6 for of loop
+//this is the easier logic to loop over arrays.
+for (arrayElem of arrayTwo) {
+  console.log(arrayElem);
+}
