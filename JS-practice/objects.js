0c5f028a2db6be11b05f9608b75c8542d8757dec
FahadShaik <shaikfahad904@gmail.com>
2024-01-31 15:25:53 +0530

    finished the examples with basic logic for create and read objects

--- JS-practice/objects.js
+++ JS-practice/objects.js
@@ -5,9 +5,18 @@ const mensClothing = {
   topwear: "shirt",
   bottomwear: "pants",
   wristwear: ["bracelets", "watch"],
-  headgear: ["hat", "helmet"],
-  footwear: "shoes",
-  eyewear: "shades",
+  headgear: {
+    hat: "being human",
+    size: 22,
+  },
+  footwear: null,
+  eyewear: undefined,
 };
 
+//read from an object
+console.log(mensClothing);
 
+//read particular property from js
+console.log(mensClothing.headgear.hat);
+console.log(mensClothing.headgear.size);
+console.log(mensClothing.topwear);
