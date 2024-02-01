813b9b6dab6739f8001bdc7452fc0e272620301a
FahadShaik <shaikfahad904@gmail.com>
2024-02-01 14:44:07 +0530

    completed loops basic assignments

---
+++ JS-practice/assignment1(1).js
@@ -0,0 +1,89 @@
+//38. Write a program to print 1 to 10 numbers using a while loop
+
+/* let i = 1;
+while (i <= 10) {
+  console.log(i);
+  i++;
+} */
+
+//39. Write a program to print 10 to 1 number using a while loop
+
+/* let i = 10;
+while (i >= 1) {
+  console.log(i);
+  i = i - 1;
+} */
+
+//40. Write a program to print 5 to 15 numbers using while Loop
+
+/* let i = 5;
+while (i <= 25) {
+  console.log(i);
+  i++;
+} */
+
+//40. Write a program to print 15 to 10 numbers using while Loop
+
+/* let i = 15;
+
+while (i >= 10) {
+  console.log(i);
+  i = i - 1;
+} */
+
+//42. Write a program to print 1 to 10 even numbers using a while loop
+
+/* let i = 0;
+while (i <= 10) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+  i++;
+} */
+
+//43. Write a program to print 1 to 10 odd numbers using while Loop
+
+/* let i = 0;
+while (i <= 10) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+  i++;
+} */
+
+//44. Write a program to print 1 to 100 numbers using a for loop
+
+/* for (let i = 1; i <= 100; i++) {
+  console.log(i);
+} */
+//45. Write a program to print 1 to 100 numbers using a for loop
+
+/* for (let i = 100; i >= 1; i--) {
+  console.log(i);
+} */
+
+//46. Write a program to print 50 to 150 numbers using For Loop
+for (let i = 50; i <= 150; i++) {
+  console.log(i);
+}
+console.log("=============");
+//47. Write a program to print 15 to 10 numbers using for Loop
+for (let i = 15; i >= 10; i--) {
+  console.log(i);
+}
+console.log("=============");
+
+//48. Write a program to print 1 to 10 even numbers using a for loop
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+console.log("=============");
+
+//49. Write a program to print 1 to 10 odd numbers using For Loop
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
