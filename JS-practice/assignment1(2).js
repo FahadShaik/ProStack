e11d64813db41fb5ea1680161abe6209bc7ce93d
FahadShaik <shaikfahad904@gmail.com>
2024-02-01 16:21:49 +0530

    test

--- JS-practice/assignment1(2).js
+++ JS-practice/assignment1(2).js
@@ -31,15 +31,15 @@ while (i <= 10) {
 
 // 60. Write a program to read 5 numbers and print only the even numbers.
 
-for (let i = 1; i <= 5; i++) {
-  let input = prompt(`enter the digit ${i} :`);
-  let digit = parseInt(input);
-  if (digit % 2 === 0) {
-    console.log("this number is even ", digit);
-  } else {
-    console.log("Not a even number");
-  }
-}
+// for (let i = 1; i <= 5; i++) {
+//   let input = prompt(`enter the digit ${i} :`);
+//   let digit = parseInt(input);
+//   if (digit % 2 === 0) {
+//     console.log("this number is even ", digit);
+//   } else {
+//     console.log("Not a even number");
+//   }
+// }
 
 // 61. Write a program to print the Fibonacci number series up to a given number.
 // Expected out 17 -: 0 1 1 2 3 5 8 13
