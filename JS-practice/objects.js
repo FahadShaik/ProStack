51e60146f2fdb2ba5da982815ecc79b72c45bb9d
FahadShaik <shaikfahad904@gmail.com>
2024-02-01 12:28:18 +0530

    Objects a little more syntax

--- JS-practice/objects.js
+++ JS-practice/objects.js
@@ -14,9 +14,21 @@ const mensClothing = {
 };
 
 //read from an object
-console.log(mensClothing);
+// console.log(mensClothing);
 
 //read particular property from js
-console.log(mensClothing.headgear.hat);
-console.log(mensClothing.headgear.size);
-console.log(mensClothing.topwear);
+// console.log(mensClothing.headgear.hat);
+// console.log(mensClothing.headgear.size);
+// console.log(mensClothing.topwear);
+//Rare way to call the object's value
+console.log(mensClothing["topwear"]);
+// print keys in array format
+console.log(Object.keys(mensClothing));
+//object's length
+console.log(Object.keys(mensClothing).length);
+
+//iterating over an object is not possible but still if we wanna do it we go for the for of loop
+
+for (key in mensClothing) {
+  console.log(key, ":", mensClothing[key]);
+}
