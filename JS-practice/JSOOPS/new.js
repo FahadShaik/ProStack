d8532c48236548db38ed90735e501f44b317e10c
FahadShaik <shaikfahad904@gmail.com>
2024-02-13 10:43:49 +0530

    This keyword and objects

---
+++ JS-practice/JSOOPS/new.js
@@ -0,0 +1,35 @@
+class Employee {
+  emplName;
+  empSal;
+
+  creatingDesigns() {
+    this.empSal = 23000;
+    console.log(this.empSal);
+    console.log("create Wireframes");
+  }
+  developingCode() {
+    this.empSal = 24000;
+    console.log(this.empSal);
+    console.log("Write code");
+  }
+  testingCode() {
+    this.empSal = 25000;
+    console.log("test Code");
+  }
+  HandlingStage() {
+    this.empSal = 26000;
+    console.log("push code to stage");
+  }
+  prodDeployment() {
+    this.empSal = 37000;
+    console.log("deploy code");
+  }
+}
+
+const designerEmp = new Employee();
+const developerEmp = new Employee();
+const testerEmp = new Employee();
+const stageEMp = new Employee();
+const prodEmp = new Employee();
+
+designerEmp.creatingDesigns();
